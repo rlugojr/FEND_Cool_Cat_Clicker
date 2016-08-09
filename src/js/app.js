cea0b9eb24279fd53e7113fec9a59e9eda5730a6
Ray Lugo, Jr <rlugojr@gmail.com>
2016-08-08 21:47:08 -0400

    Added ViewModel

---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+
+
+
+    coolCatData = {
+        coolCats: [
+                {coolCatName: 'deniro',
+                coolCatCount: 0,
+                coolCatImage: 'img/deniro.jpg'},
+                {coolCatName: 'Keitel',
+                coolCatCount: 0,
+                coolCatImage: 'img/keitel.jpg'},
+                {coolCatName: 'Pacino',
+                coolCatCount: 0,
+                coolCatImage: 'img/Pacino.jpg'}
+            ]
+    }
+
+
+    var ViewModel = function(){
+        this.clickCount = ko.observable(0);
+        this.name = ko.observable('DeNiro');
+        this.imgSrc = ko.observable('img/deniro.jpg');
+
+        this.incrementCounter = function(){
+            this.clickcount(this.clickCount + 1);
+        };
+        
+    }
+
+    ko.applyBindings(new ViewModel());
+
