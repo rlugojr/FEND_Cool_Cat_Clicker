d9c90fd3d9019f8fd2bdb7b7dbcf2db98a30198d
Ray Lugo, Jr <rlugojr@gmail.com>
2016-08-09 22:10:05 -0400

    Linted and Prettified
    As advised by Karol
    (https://discussions.udacity.com/users/Karol/activity), as well as the
    other changes he suggested made since the first release, thanks to his
    code review I am a better developer than I was yesterday.

--- js/app.js
+++ js/app.js
@@ -1,46 +1,47 @@
+var coolCatData = [
+    {
+        name: 'De Niro',
+        clickCount: 0,
+        movies: ['GodFather II', 'Raging Bull', "GoodFellas", "Taxi Driver"],
+        imgSrc: 'img/deniro.jpg'
+    },
+    {
+        name: 'Keitel',
+        clickCount: 0,
+        movies: ['Reservoir Dogs', 'Pulp Fiction', 'Mean Streets'],
+        imgSrc: 'img/keitel.jpg'
+    },
+    {
+        name: 'Oldman',
+        clickCount: 0,
+        movies: ['Dracula', 'True Romance', 'Rozencrantz and Guildenstern are dead', 'Nolan\'s Batman Trilogy'],
+        imgSrc: 'img/oldman.jpg'
+    },
+    {
+        name: 'Pacino',
+        clickCount: 0,
+        movies: ['GodFather Trilogy', 'Scarface', 'Carlito\'s way'],
+        imgSrc: 'img/pacino.jpg'
+    },
+    {
+        name: 'Walken',
+        clickCount: 0,
+        movies: ['True Romance', 'The Deerhunter', 'The King of NY'],
+        imgSrc: 'img/walken.jpg'
+    }
+];
 
-var coolCatData =  [
-                {
-                    name: 'De Niro',
-                    clickCount: 0,
-                    movies: ['GodFather II','Raging Bull', "GoodFellas", "Taxi Driver"],
-                    imgSrc: 'img/deniro.jpg'
-                },
-                {
-                    name: 'Keitel',
-                    clickCount: 0,
-                    movies: ['Reservoir Dogs','Pulp Fiction','Mean Streets'],
-                    imgSrc: 'img/keitel.jpg'
-                },
-                {
-                    name: 'Oldman',
-                    clickCount: 0,
-                    movies: ['Dracula','True Romance','Rozencrantz and Guildenstern are dead', 'Nolan\'s Batman Trilogy'],
-                    imgSrc: 'img/oldman.jpg'
-                },
-                {
-                    name: 'Pacino',
-                    clickCount: 0,
-                    movies: ['GodFather Trilogy','Scarface','Carlito\'s way'],
-                    imgSrc: 'img/pacino.jpg'
-                },
-                {
-                    name: 'Walken',
-                    clickCount: 0,
-                    movies: ['True Romance','The Deerhunter','The King of NY'],
-                    imgSrc: 'img/walken.jpg'
-                }
-            ]
 
+var CoolCat = function(data) {
+    var self = this;
 
-var CoolCat = function(data){
     this.clickCount = ko.observable(data.clickCount);
     this.name = ko.observable(data.name);
-    this.level = ko.computed(function(){
-        var clicks = Math.floor(this.clickCount()/5);
-        switch(clicks){
+    this.level = ko.computed(function() {
+        var clicks = Math.floor(self.clickCount() / 5);
+        switch (clicks) {
             case 0:
-                return "Street Thug"
+                return "Street Thug";
             case 1:
                 return "Friend of Ours";
             case 2:
@@ -48,34 +49,34 @@ var CoolCat = function(data){
             case 3:
                 return "Capo";
             case 4:
+            		return "Enforcer";
             default:
                 return "Capo di Tutti Capo";
-        }; 
+        }
     }, this);
     this.imgSrc = ko.observable(data.imgSrc);
     this.movies = ko.observable(data.movies);
-}
+};
 
-var ViewModel = function(){
+var ViewModel = function() {
     var self = this;
 
     this.coolCatList = ko.observableArray([]);
 
-    coolCatData.forEach(function(coolCatItem){
+    coolCatData.forEach(function(coolCatItem) {
         self.coolCatList.push(new CoolCat(coolCatItem));
     });
 
     this.currentCoolCat = ko.observable(this.coolCatList()[0]);
 
-    this.incrementCounter = function(){
+    this.incrementCounter = function() {
         self.currentCoolCat().clickCount(self.currentCoolCat().clickCount() + 1);
     };
 
-    this.swapCat = function(clickedCoolCat){
-        self.currentCoolCat(clickedCoolCat)
+    this.swapCat = function(clickedCoolCat) {
+        self.currentCoolCat(clickedCoolCat);
     };
 
-}
+};
 
 ko.applyBindings(new ViewModel());
-
